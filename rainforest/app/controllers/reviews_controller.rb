ce580f6d961f224b56603c28b7c1385af075d7b9
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-11-26 17:05:43 -0500

    error in JS fixed beacuse of missing end of input. review loop wrapped into div

--- rainforest/app/controllers/reviews_controller.rb
+++ rainforest/app/controllers/reviews_controller.rb
@@ -1,46 +1,40 @@
 class ReviewsController < ApplicationController
-	
-	before_filter :ensure_logged_in, only: [:create, :destroy]
-	before_filter :load_product
+  	
+  	before_filter :ensure_logged_in, only: [:create, :destroy]
+  	before_filter :load_product
 
-  def show
-  	@review = Review.find(params[:id])
-  end
+    def show
+    	@review = Review.find(params[:id])
+    end
 
-  def create
-  	@review = @product.reviews.build(review_params)
-  	@review.user = current_user
+    def create
+    	@review = @product.reviews.build(review_params)
+    	@review.user = current_user
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to product_path(@product.id), notice: 'Reivew added.' }
-        format.js {} 
-      else
-        format.html { render 'products/show', alert: 'There was an error.' }
-        format.js {}
+      respond_to do |format|
+        if @review.save
+          format.html { redirect_to product_path(@product.id), notice: 'Review added.' }
+          format.js {} 
+        else
+          format.html { render 'products/show', alert: 'There was an error.' }
+          format.js {}
+        end
       end
+    end
 
-  	# if @review.save
-  	# 	redirect_to products_path, notice: 'Review created successfully'
-  	# else
-  	# 	render 'products/show'
-  	# end
+    def destroy
+    	@review = Review.find(params[:id])
+    	@review.destroy
+    end
+
+    private
+    def review_params
+    	params.require(:review).permit(:comment, :product_id)
+    end
+
+    def load_product
+    	@product = Product.find(params[:product_id])
+    end
+
   end
-
-  def destroy
-  	@review = Review.find(params[:id])
-  	@review.destroy
-  end
-
-  private
-  def review_params
-  	params.require(:review).permit(:comment, :product_id)
-  end
-
-  def load_product
-  	@product = Product.find(params[:product_id])
-  end
-
-end
-
 
