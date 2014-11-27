e03ee676f9547ef10abde041da675343921af75e
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-11-26 20:47:10 -0500

    fixed mix up in bootstrap gems. currently there is no navbar and bootstrap isnt full implemeted-no errors

--- rainforest/app/assets/javascripts/application.js
+++ rainforest/app/assets/javascripts/application.js
@@ -12,6 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+//= require_self
+
+
+jQuery(function() {
+  $("a[rel~=popover], .has-popover").popover();
+  $("a[rel~=tooltip], .has-tooltip").tooltip();
+});
