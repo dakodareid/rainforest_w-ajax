0b27de9b48a01c1374fd16349ab0449dac931700
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-11-27 15:38:12 -0500

    animation on main image completed! wooo

--- rainforest/app/assets/javascripts/products.js
+++ rainforest/app/assets/javascripts/products.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function() {
+$(document).on('ready page:load', function() {
 
 	$('#search-form').submit(function(event) {
 		event.preventDefault();
